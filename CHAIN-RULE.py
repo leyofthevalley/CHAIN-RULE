b9268fce0f2cbbdc5f4cfc683e5f2c6954817f77
Ryuuiii <andalmarryjoy@gmail.com>
2025-05-15 21:16:08 +0800

    Change the position of buttons

--- CHAIN-RULE.py
+++ CHAIN-RULE.py
@@ -1,5 +1,5 @@
 #To run the code. First, install the sympy by typing 'pip install sympy' on the terminal.
-#Make sure to have python
+#Make sure to have python first.
 
 import tkinter as tk
 from tkinter import ttk, messagebox
